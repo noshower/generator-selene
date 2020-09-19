bb8281fc57e9519f085d719460915512c58b874a
吴震宇 <wu.zhenyu@darcytech.com>
2020-09-19 21:44:30 +0800

    修改配置

--- generators/templates/stylelint.config.js
+++ generators/templates/stylelint.config.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties', 'stylelint-declaration-use-variable'],
   rules: {
     'plugin/declaration-block-no-ignored-properties': true,
-    'sh-waqar/declaration-use-variable': [['/color/', 'padding', 'margin']],
+    'sh-waqar/declaration-use-variable': [['/color/', '/padding/', '/margin/', { ignoreValues: ['0', 'transparent', 'white', 'black'] }]],
   },
   ignoreFiles: ['/style/color/{bezierEasing,colorPalette,tinyColor}.less'],
 };
